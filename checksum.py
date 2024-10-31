d24dd9c515ce61749b3c165ab4375615c9453d4f
Cinderleaf <93451672+CinderleafFFE@users.noreply.github.com>
2024-10-31 23:21:18 +0800

    use json instead

--- checksum.py
+++ checksum.py
@@ -1,18 +1,23 @@
 import os
 import hashlib
+import json
 assets_dirs = ["Graphics", "Audio"]
-with open("assets_sum.txt", "w") as sum_file:
-    os.chdir("assets")
-    for assets_dir in assets_dirs:
-        for root, dirs, files in os.walk(assets_dir):
-            for name in files:
-                fname = os.path.join(root, name)
-                hasher = hashlib.md5()
-                with open(fname, 'rb') as ass:
-                    while True:
-                        data = ass.read(4096)
-                        if len(data) == 0:
-                            break
-                        hasher.update(data)
-                md5 = hasher.hexdigest()
-                sum_file.write('"' + fname + '"' + ' ' + md5 + '\n')
+os.chdir("assets")
+md5_dict = {}
+for assets_dir in assets_dirs:
+    for root, dirs, files in os.walk(assets_dir):
+        for name in files:
+            fname = os.path.join(root, name)
+            hasher = hashlib.md5()
+            with open(fname, 'rb') as ass:
+                while True:
+                    data = ass.read(4096)
+                    if len(data) == 0:
+                        break
+                    hasher.update(data)
+            md5 = hasher.hexdigest()
+            md5_dict.update({fname : md5})
+os.chdir("..")
+fp = open("assets_sum.json", "w")
+json.dump(md5_dict, fp)
+fp.close()
